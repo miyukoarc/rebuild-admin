0f61d89d21a8281e827d919972bf7735a67d2ff7
13979827077 <1251607491@qq.com>
2020-04-24 09:32:09 +0800

    4/24

--- src/views/management/employee/index.vue
+++ src/views/management/employee/index.vue
@@ -1,24 +1,23 @@
 <template>
   <el-container>
-      <el-aside width="200px" class="p-3">
-          <el-input
-            placeholder="输入关键字进行过滤"
-            v-model="filterText"
-            size="small"
-            >
-        </el-input>
-        <m-tree :datalist='datalist' class="mt-2"/>
-      </el-aside>
-      <el-container>
-          <el-header height="">
-              <!-- Header content -->
-          </el-header>
-          <el-main height="">
-              <mTable/>
-          </el-main>
-      </el-container>
+    <el-aside width="200px" class="p-3">
+      <el-input
+        placeholder="输入关键字进行过滤"
+        v-model="filterText"
+        size="small"
+      >
+      </el-input>
+      <m-tree :datalist="datalist" class="mt-2" />
+    </el-aside>
+    <el-container>
+      <el-header height="">
+        <!-- Header content -->
+      </el-header>
+      <el-main height="">
+        <mTable :columns="columns" />
+      </el-main>
+    </el-container>
   </el-container>
-  
 </template>
 
 <script>
@@ -27,7 +26,7 @@ import mTable from "@/components/CommonTable";
 export default {
   components: {
     mTree,
-    mTable,
+    mTable
   },
   data() {
     return {
@@ -38,7 +37,7 @@ export default {
             {
               label: "运营部"
             },
-             {
+            {
               label: "技术部"
             }
           ]
@@ -49,7 +48,7 @@ export default {
             {
               label: "运营部"
             },
-             {
+            {
               label: "技术部"
             }
           ]
@@ -60,10 +59,48 @@ export default {
             {
               label: "运营部"
             },
-             {
+            {
               label: "运维部"
             }
           ]
+        }
+      ],
+      columns: [
+        {
+          visible: true,
+          label: "姓名",
+          prop: "name",
+          uuid: 1,
+          align: "center"
+        },
+        {
+          visible: true,
+          label: "职务",
+          prop: "name",
+          uuid:2,
+          align: "center"
+        },
+        ,
+        {
+          visible: true,
+          label: "部门",
+          prop: "name",
+          uuid: 3,
+          align: "center"
+        },
+        {
+          visible: true,
+          label: "手机",
+          prop: "phone",
+          uuid: 4,
+          align: "center"
+        },
+        {
+          visible: true,
+          label: "邮箱",
+          prop: "email",
+          uuid: 5,
+          align: "center"
         }
       ]
     };
